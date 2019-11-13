17c9709fcef3761f3590599c80371a7663d67038
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-11-13 20:30:54 +0900

    Refine code parser (#5)
    * Changed: story filter by priority
    
    * Changed: pronoun replaced
    
    * Changed: tag replaced
    
    * Changed: layer replaced

--- builder/parser.py
+++ builder/parser.py
@@ -10,6 +10,7 @@ from .episode import Episode
 from .scene import Scene, ScenarioType
 from .action import Action, ActType, TagAction, TagType
 from .who import Who
+from .person import Person
 from .description import Description, DescType, NoDesc
 from .basesubject import NoSubject
 from .strutils import str_replaced_tag_by_dictionary, str_duplicated_chopped, extraspace_chopped, duplicate_bracket_chop_and_replaceed
@@ -19,6 +20,7 @@ from .utils import strOfDescription
 # define type hint
 AllActions = [Action, CombAction, TagAction]
 BaseActions = [Action, TagAction]
+Someone = [Person, NoSubject]
 
 
 class Parser(object):
@@ -52,16 +54,16 @@ class Parser(object):
                 4) description connected
                 5) tag replaced
         '''
-        return story_tag_replaced(
+        return _tagsReplacedFrom(
                 _descriptionConnectedFrom(
-                    story_pronoun_replaced(
-                        story_layer_replaced(
-                            story_filtered_by_priority(story, pri_filter)
+                    _pronounReplacedFrom(
+                        _layerReplacedFrom(
+                            _storyFilteredFrom(story, pri_filter)
                     ))), words
                 )
 
 # privates
-def _actionLayersFrom(story: Story):
+def _actionLayersFrom(story: Story) -> list:
     def _in_action(action: AllActions, head: str):
         if isinstance(action, CombAction):
             return [(v.layer, head, v) for v in action.actions]
@@ -80,7 +82,7 @@ def _actionLayersFrom(story: Story):
     return [(f"# {story.title}\n")] \
             + list(chain.from_iterable(_in_chapter(v) for v in story.chapters))
 
-def _descriptionConnectedFrom(story: Story):
+def _descriptionConnectedFrom(story: Story) -> Story:
     def _in_action(action: AllActions):
         if isinstance(action, CombAction):
             return action.inherited(*[_in_action(v) for v in action.actions])
@@ -102,7 +104,7 @@ def _descriptionConnectedFrom(story: Story):
 
     return story.inherited(*[_in_chapter(v) for v in story.chapters])
 
-def _descriptionsFrom(story: Story, is_comment: bool):
+def _descriptionsFrom(story: Story, is_comment: bool) -> list:
     def _in_action(action: AllActions, is_comment: bool):
         if isinstance(action, CombAction):
             return [str_duplicated_chopped(
@@ -135,7 +137,42 @@ def _descriptionsFrom(story: Story, is_comment: bool):
     return [f"# {story.title}\n"] \
             + list(chain.from_iterable(_in_chapter(v, is_comment) for v in story.chapters))
 
-def _outlinesFrom(story: Story):
+def _layerReplacedFrom(story: Story):
+    def _in_episode(episode: Episode):
+        return episode.inherited(*[_layerReplacedInScene(v) for v in episode.scenes])
+
+    def _in_chapter(chapter: Chapter):
+        return chapter.inherited(*[_in_episode(v) for v in chapter.episodes])
+
+    return story.inherited(*[_in_chapter(v) for v in story.chapters])
+
+def _layerReplacedInScene(scene: Scene):
+    tmp = []
+    cur = Action.MAIN_LAYER
+
+    def _sel_layer(act: Action, cur: str):
+        return act.setLayer(cur) if act.layer == Action.DEF_LAYER else act
+
+    def _in_combaction(act: CombAction, cur):
+        c_tmp = []
+        for a in act.actions:
+            if isinstance(a, TagAction) and a.tag_type is TagType.SET_LAYER:
+                cur = a.info
+            else:
+                c_tmp.append(_sel_layer(a, cur))
+        return act.inherited(*c_tmp), cur
+
+    for v in scene.actions:
+        if isinstance(v, CombAction):
+            c_tmp, cur = _in_combaction(v, cur)
+            tmp.append(c_tmp)
+        elif isinstance(v, TagAction) and v.tag_type is TagType.SET_LAYER:
+            cur = v.info
+        else:
+            tmp.append(_sel_layer(v, cur))
+    return scene.inherited(*tmp)
+
+def _outlinesFrom(story: Story) -> list:
     def _in_scene(scene: Scene):
         return [(f"- {scene.title}", f"{scene.outline}")]
 
@@ -150,7 +187,40 @@ def _outlinesFrom(story: Story):
     return [(f"# {story.title}", "\n")] \
             + list(chain.from_iterable(_in_chapter(v) for v in story.chapters))
 
-def _scenariosFrom(story: Story, is_comment: bool):
+def _pronounReplacedFrom(story: Story) -> Story:
+    def _in_episode(episode: Episode):
+        return episode.inherited(*[_pronounReplacedInScene(v) for v in episode.scenes])
+
+    def _in_chapter(chapter: Chapter):
+        return chapter.inherited(*[_in_episode(v) for v in chapter.episodes])
+
+    return story.inherited(*[_in_chapter(v) for v in story.chapters])
+
+def _pronounReplacedInScene(scene: Scene) -> Scene:
+    tmp = []
+    cur_sub = NoSubject()
+    for a in scene.actions:
+        if isinstance(a, CombAction):
+            tmp_c = []
+            for v in a.actions:
+                if isinstance(v, TagAction):
+                    tmp_c.append(v)
+                elif isinstance(v.subject, Who):
+                    tmp_c.append(v.inherited(subject=cur_sub))
+                else:
+                    cur_sub = v.subject
+                    tmp_c.append(v)
+            tmp.append(a.inherited(*tmp_c))
+        elif isinstance(a, TagAction):
+            tmp.append(a)
+        elif isinstance(a.subject, Who):
+            tmp.append(a.inherited(subject=cur_sub))
+        else:
+            cur_sub = a.subject
+            tmp.append(a)
+    return scene.inherited(*tmp)
+
+def _scenariosFrom(story: Story, is_comment: bool) -> list:
     def _conv(action: Action):
         if action.act_type is ActType.TALK:
             return [(ScenarioType.DIALOGUE,
@@ -183,6 +253,37 @@ def _scenariosFrom(story: Story, is_comment: bool):
     return [(ScenarioType.TITLE, f"# {story.title}\n")] \
             + list(chain.from_iterable(_in_chapter(v, is_comment) for v in story.chapters))
 
+def _storyFilteredFrom(story: Story, pri_filter: int) -> Story:
+    def _in_combaction(action: CombAction, pri_filter: int):
+        tmp = []
+        for v in action.actions:
+            if isinstance(v, TagAction):
+                tmp.append(v)
+            else:
+                if v.priority >= pri_filter:
+                    tmp.append(v)
+        return action.inherited(*tmp)
+
+    def _in_scene(scene: Scene, pri_filter: int):
+        tmp = []
+        for v in scene.actions:
+            if isinstance(v, CombAction):
+                tmp.append(_in_combaction(v, pri_filter))
+            elif isinstance(v, TagAction):
+                tmp.append(v)
+            else:
+                if v.priority >= pri_filter:
+                    tmp.append(v)
+        return scene.inherited(*tmp)
+
+    def _in_episode(episode: Episode, pri_filter: int):
+        return episode.inherited(*[_in_scene(v, pri_filter) for v in episode.scenes if v.priority >= pri_filter])
+
+    def _in_chapter(chapter: Chapter, pri_filter: int):
+        return chapter.inherited(*[_in_episode(v, pri_filter) for v in chapter.episodes if v.priority >= pri_filter])
+
+    return story.inherited(*[_in_chapter(v, pri_filter) for v in story.chapters if v.priority >= pri_filter])
+
 def _tagActionConverted(action: TagAction, is_comment: bool) -> str:
     if action.tag_type is TagType.COMMENT and is_comment:
         return f"<!--{action.info}-->"
@@ -198,137 +299,49 @@ def _tagActionConverted(action: TagAction, is_comment: bool) -> str:
     else:
         return ""
 
-
-
-def story_filtered_by_priority(story: Story, pri_filter: int):
-    return story.inherited(*[_story_chapter_filtered(v, pri_filter) for v in story.chapters if v.priority >= pri_filter])
-
-def story_pronoun_replaced(story: Story):
-    return story.inherited(*[_story_pronoun_replaced_in_chapter(v) for v in story.chapters])
-
-def story_tag_replaced(story: Story, words: dict):
-    return story.inherited(*[_story_tag_replaced_in_chapter(v, words) for v in story.chapters])
-
-def story_layer_replaced(story: Story):
-    def _as_episode(episode: Episode):
-        return episode.inherited(*[_layer_replaced_in_scene(v) for v in episode.scenes])
-    def _as_chapter(chapter: Chapter):
-        return chapter.inherited(*[_as_episode(v) for v in chapter.episodes])
-    return story.inherited(*[_as_chapter(v) for v in story.chapters])
-
-# privates
-def _desc_in_tagaction(action: TagAction, is_comment: bool) -> list:
-    if action.tag_type is TagType.COMMENT and is_comment:
-        return [f"<!--{action.info}-->"]
-    elif action.tag_type is TagType.BR:
-        return ["\n\n"]
-    elif action.tag_type is TagType.HR:
-        return ["----" * 8]
-    elif action.tag_type is TagType.SYMBOL:
-        return [f"\n{action.info}\n"]
-    elif action.tag_type is TagType.TITLE:
-        num = int(action.subinfo)
-        return ["{} {}".format("#" * num, action.info)]
-    else:
-        return []
-
-def _layer_replaced_in_scene(scene: Scene):
-    tmp = []
-    cur = Action.MAIN_LAYER
-    def _sel_layer(act: Action, cur: str):
-        return act.setLayer(cur) if act.layer == Action.DEF_LAYER else act
-    def _as_combaction(act: CombAction, cur):
-        c_tmp = []
-        for a in act.actions:
-            if isinstance(a, TagAction) and a.tag_type is TagType.SET_LAYER:
-                cur = a.info
-            else:
-                c_tmp.append(_sel_layer(a, cur))
-        return act.inherited(*c_tmp), cur
-    for v in scene.actions:
-        if isinstance(v, CombAction):
-            c_tmp, cur = _as_combaction(v, cur)
-            tmp.append(c_tmp)
-        elif isinstance(v, TagAction) and v.tag_type is TagType.SET_LAYER:
-            cur = v.info
+def _tagsReplacedFrom(story: Story, words: dict) -> Story:
+    def _in_action(action: AllActions, words: dict):
+        if isinstance(action, CombAction):
+            return action.inherited(*[_in_action(v, words) for v in action.actions])
+        elif isinstance(action, TagAction):
+            return action
         else:
-            tmp.append(_sel_layer(v, cur))
-    return scene.inherited(*tmp)
+            return _tagReplacedInAction(action, words)
 
-def _story_chapter_filtered(chapter: Chapter, pri_filter: int):
-    return chapter.inherited(*[_story_episode_filtered(v, pri_filter) for v in chapter.episodes if v.priority >= pri_filter])
+    def _in_scene(scene: Scene, words: dict):
+        return scene.inherited(*[_in_action(v, words) for v in scene.actions])
 
-def _story_episode_filtered(episode: Episode, pri_filter: int):
-    return episode.inherited(*[_story_scene_filtered(v, pri_filter) for v in episode.scenes if v.priority >= pri_filter])
+    def _in_episode(episode: Episode, words: dict):
+        return episode.inherited(*[_in_scene(v, words) for v in episode.scenes])
 
-def _story_scene_filtered(scene: Scene, pri_filter: int):
-    tmp = []
-    def _as_combaction(act: CombAction, pri: int):
-        return act.inherited(*[v for v in act.actions if v.priority >= pri])
-    for v in scene.actions:
-        if isinstance(v, CombAction):
-            tmp.append(_as_combaction(v, pri_filter))
-        elif v.priority >= pri_filter:
-            tmp.append(v)
-    return scene.inherited(*tmp)
+    def _in_chapter(chapter: Chapter, words: dict):
+        return chapter.inherited(*[_in_episode(v, words) for v in chapter.episodes])
 
-def _story_pronoun_replaced_in_chapter(chapter: Chapter):
-    return chapter.inherited(*[_story_pronoun_replaced_in_episode(v) for v in chapter.episodes])
+    return story.inherited(*[_in_chapter(v, words) for v in story.chapters])
 
-def _story_pronoun_replaced_in_episode(episode: Episode):
-    return episode.inherited(*[_story_pronoun_replaced_in_scene(v) for v in episode.scenes])
-
-def _story_pronoun_replaced_in_scene(scene: Scene):
-    tmp = []
-    cur_sub = NoSubject()
-    for a in scene.actions:
-        if isinstance(a, CombAction):
-            tmp_c = []
-            for v in a.actions:
-                if isinstance(v, TagAction):
-                    tmp_c.append(v)
-                elif isinstance(v.subject, Who):
-                    tmp_c.append(v.inherited(subject=cur_sub))
-                else:
-                    cur_sub = v.subject
-                    tmp_c.append(v)
-            tmp.append(a.inherited(*tmp_c))
-        elif isinstance(a, TagAction):
-            tmp.append(a)
-        elif isinstance(a.subject, Who):
-            tmp.append(a.inherited(subject=cur_sub))
-        else:
-            cur_sub = a.subject
-            tmp.append(a)
-    return scene.inherited(*tmp)
-
-def _story_tag_replaced_in_chapter(chapter: Chapter, words: dict):
-    return chapter.inherited(*[_story_tag_replaced_in_episode(v, words) for v in chapter.episodes])
-
-def _story_tag_replaced_in_episode(episode: Episode, words: dict):
-    return episode.inherited(*[_story_tag_replaced_in_scene(v, words) for v in episode.scenes])
-
-def _story_tag_replaced_in_scene(scene: Scene, words: dict):
-    def _as_combaction(act: CombAction, words: dict):
-        return act.inherited(*[_story_tag_replaced_in_action(v, words) for v in act.actions])
-    return scene.inherited(*[_as_combaction(v, words) if isinstance(v, CombAction) else _story_tag_replaced_in_action(v, words) for v in scene.actions])
-
-def _story_tag_replaced_in_action(act: [Action, TagAction], words: dict):
-    if isinstance(act, TagAction):
-        return act
+def _tagReplacedInAction(act: Action, words: dict) -> Action:
     is_nodesc = isinstance(act.description, NoDesc)
     outline = act.outline
-    desc = NoDesc() if is_nodesc else "".join(act.description.descs)
+    desc = NoDesc() if is_nodesc else strOfDescription(act)
+
     if hasattr(act.subject, "calling"):
         outline = str_replaced_tag_by_dictionary(act.outline, act.subject.calling)
         if not is_nodesc:
             desc = str_replaced_tag_by_dictionary(desc, act.subject.calling)
+
     outline = str_replaced_tag_by_dictionary(outline, words)
+
     if not is_nodesc:
         desc = str_replaced_tag_by_dictionary(desc, words)
-    if _isInvalidatedTagReplaced(outline): raise AssertionError("Cannot convert tags in:", outline)
-    if not is_nodesc and _isInvalidatedTagReplaced(desc): raise AssertionError("Cannot convert tags in:", desc)
+
+    if _isInvalidatedTagReplaced(outline):
+        raise AssertionError("Cannot convert tags in:", outline)
+
+    if not is_nodesc and _isInvalidatedTagReplaced(desc):
+        raise AssertionError("Cannot convert tags in:", desc)
+
     return act.inherited(outline=outline, desc=desc)
+
 
 ## checker
 def _isInvalidatedTagReplaced(target: str, prefix: str='$'):
