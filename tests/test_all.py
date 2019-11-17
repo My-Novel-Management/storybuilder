6c6d76d362524ae54c698dc1df6a9f99f9ea35e4
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-11-17 20:23:16 +0900

    Reorder: import and test order in alltest (#76)
    #54 resolved

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
 """Test suite for all tests
 """
+## official library
 import unittest
+## local files
+import test_action
+import test_analyzer
 import test_assertion
 import test_basecontainer
 import test_basedata
 import test_basesubject
-import test_chara
-import test_episode
-import test_it
-import test_item
-import test_story
-import test_strutils
-import test_world
+import test_buildtool
 import test_chapter
-import test_scene
-import test_stage
-import test_day
-import test_time
-import test_person
-import test_action
-import test_description
+import test_chara
 import test_combaction
+import test_day
+import test_description
+import test_episode
 import test_flag
 import test_formatter
+import test_it
+import test_item
+import test_parser
+import test_person
+import test_scene
+import test_stage
+import test_story
+import test_strutils
+import test_time
 import test_utils
 import test_who
 import test_word
-import test_analyzer
-import test_parser
-import test_buildtool
+import test_world
 
 
 def suite():
@@ -49,46 +51,38 @@ def suite():
         unittest.makeSuite(test_formatter.FormatterTest),
         unittest.makeSuite(test_strutils.MethodsTest),
         unittest.makeSuite(test_utils.MethodsTest),
-        # tools
-        unittest.makeSuite(test_analyzer.AnalyzerTest),
-        unittest.makeSuite(test_buildtool.BuildTest),
-        unittest.makeSuite(test_parser.ParserTest),
         # data type
+        unittest.makeSuite(test_chara.CharaTest),
+        unittest.makeSuite(test_day.DayTest),
         unittest.makeSuite(test_flag.FlagTest),
         unittest.makeSuite(test_flag.NoFlagTest),
         unittest.makeSuite(test_flag.NoDeflagTest),
         unittest.makeSuite(test_it.ItTest),
         unittest.makeSuite(test_item.ItemTest),
-        unittest.makeSuite(test_story.StoryTest),
+        unittest.makeSuite(test_person.PersonTest),
+        unittest.makeSuite(test_stage.StageTest),
+        unittest.makeSuite(test_time.TimeTest),
         unittest.makeSuite(test_who.WhoTest),
         unittest.makeSuite(test_who.WhenTest),
         unittest.makeSuite(test_who.WhereTest),
         unittest.makeSuite(test_word.WordTest),
-        ## stage
-        unittest.makeSuite(test_stage.StageTest),
-        ## day
-        unittest.makeSuite(test_day.DayTest),
-        ## time
-        unittest.makeSuite(test_time.TimeTest),
-        ## person
-        unittest.makeSuite(test_person.PersonTest),
-        unittest.makeSuite(test_chara.CharaTest),
-        ## action
+        # container type
         unittest.makeSuite(test_action.ActionTest),
         unittest.makeSuite(test_action.TagActionTest),
-        ## description
+        unittest.makeSuite(test_chapter.ChapterTest),
+        unittest.makeSuite(test_combaction.CombActionTest),
         unittest.makeSuite(test_description.DescriptionTest),
         unittest.makeSuite(test_description.NoDescTest),
-        # container type
-        ## chapter
-        unittest.makeSuite(test_chapter.ChapterTest),
         unittest.makeSuite(test_episode.EpisodeTest),
         unittest.makeSuite(test_scene.SceneTest),
+        unittest.makeSuite(test_story.StoryTest),
         # world
         unittest.makeSuite(test_world.UtilityDictTest),
         unittest.makeSuite(test_world.WorldTest),
-        ## container
-        unittest.makeSuite(test_combaction.CombActionTest),
+        # tools
+        unittest.makeSuite(test_analyzer.AnalyzerTest),
+        unittest.makeSuite(test_buildtool.BuildTest),
+        unittest.makeSuite(test_parser.ParserTest),
         ))
 
     return suite
