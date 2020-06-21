3a05e1a15cc55d378b4900d172a2beec34e2a736
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-06-21 11:15:53 +0900

    Fixed: import typo (#298)
    * Fixed: import typo
    
    * Updated: v0.5.9-3[ci skip]

--- builder/utils/util_dict.py
+++ builder/utils/util_dict.py
@@ -17,7 +17,7 @@ from builder.utils import assertion
 def calling_dict_from(calling: (str, dict), name: str) -> dict:
     ''' Construct a calling dictionary for Person class.
     '''
-    from utils.util_str import dict_from_string
+    from builder.utils.util_str import dict_from_string
     tmp = {}
     if isinstance(calling, dict):
         tmp = calling
