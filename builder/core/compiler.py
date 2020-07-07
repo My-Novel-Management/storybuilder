6f9c614f889a94e0a0e73e37d09ea28278ed16a9
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-07-07 22:26:02 +0900

    Fix typo (#347)

--- builder/core/compiler.py
+++ builder/core/compiler.py
@@ -413,7 +413,7 @@ class Compiler(Executer):
         tmp = []
         data = assertion.is_instance(src, SCode).script[0]
         tmp.append('# Information')
-        tmp.append(f'【コピィ】\n\{data["copy"]}n')
+        tmp.append(f'【コピィ】\n{data["copy"]}\n')
         tmp.append(f'【ひとこと】\n{data["oneline"]}\n')
         tmp.append(f'【あらすじ】\n{data["outline"]}\n')
         if data["contest_info"]:
